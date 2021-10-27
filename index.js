ea3a8bb8ee773bfc68f0da3a2af678c3e28804e8
Minh Nguyen <63439671+minhh2792@users.noreply.github.com>
2021-10-27 14:25:37 +0700

    toxic

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ client.on('message', async (message) => {
 
     const snipe = snipes[channelID];
 
-    if (!snipe) return message.channel.send("Hong có gì ở đây ._.");
+    if (!snipe) return message.channel.send("deo co gi o day");
 
     const embed = new MessageEmbed()
       .setAuthor(snipe.author.tag, snipe.author.displayAvatarURL())
@@ -64,7 +64,7 @@ client.on('message', async (message) => {
 
     const snipe = editSnipes[channelID];
 
-    if (!snipe) return message.channel.send("Hong có gì ở đây ._.");
+    if (!snipe) return message.channel.send("deo co gi o day");
 
     const embed = new MessageEmbed()
       .setAuthor(snipe.author.tag, snipe.author.displayAvatarURL())
